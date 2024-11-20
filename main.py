f80389f06d530697d9723e67feb8b2c9f3e23156
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2024-11-20 14:29:52 +0100

    cors settings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, os
 from turtle import dot
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from sqlalchemy import desc
 
@@ -10,8 +11,6 @@ from src.plugins.user import controller as user_controller
 from src.plugins.kpi import controller as kpi_controller
 from src.plugins.report import controller as report_controller
 from utils import description
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-
 from src.config.firebase_config import initialize_firebase
 
 import logging
@@ -23,8 +22,6 @@ logger = logging.getLogger(__name__)
 initialize_firebase()
 
 API_VERSION = os.getenv("VERSION")
-print(API_VERSION)
-print(API_VERSION)
 
 app = FastAPI(
     title="Industry 5.0 RESTful API",
@@ -34,8 +31,22 @@ app = FastAPI(
     redoc_url=f"/api/{API_VERSION}/redoc",
 )
 
-# Add this in production to force https redirection
-#app.add_middleware(HTTPSRedirectMiddleware)
+# CORS settings for local development
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", summary="Redirect to Swagger docs")
 async def redirect_to_docs():
