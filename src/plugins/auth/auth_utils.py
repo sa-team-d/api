f6829050185fc03f2f66837aa774f6e8a5f98e10
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2024-11-23 13:39:24 +0100

    refactor

--- src/plugins/auth/auth_utils.py
+++ src/plugins/auth/auth_utils.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
+debug_mode = os.getenv("DEBUG")
 
 def get_id_token(email: str, password: str):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}"
@@ -17,7 +18,7 @@ def get_id_token(email: str, password: str):
     response_data = response.json()
 
     if "idToken" in response_data:
-        print(f"User {email} signed in successfully with ID token: {response_data['idToken']}")
+        if debug_mode: print(f"User {email} signed in successfully with ID token: {response_data['idToken']}")
         return response_data["idToken"]
     else:
         raise Exception(f"Error signing in: {response_data.get('error', {}).get('message', 'Unknown error')}")
